5057a707e1a880b6ff18e3b86188489ec0c1ab29
Kirankumar <kumarsv.kiran@gmail.com>
2024-05-16 19:17:20 +0530

    Updated codes in requirements and setup.py files v1

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import find_packages, setup
 
+def get_requirements()->list[str]:
+    with open('requirements.txt') as f:
+        requirements_list = f.read().splitlines()
+    return requirements_list
+
 
 setup(
 name='sensor',
@@ -7,4 +12,5 @@ version='0.0.1',
 author='Kiran',
 author_email='kumarsv.kiran@gmail.com',
 packages=find_packages(),
+install_requires = get_requirements()
 )
